87ecc2fae7d3f3cfa29af02a70ef5dd29d318a5c
ESClaus <eric@clausgames.com>
2026-05-10 14:33:46 -0600

    Suppress session restore dialog by correcting exit type on startup

--- rootfs/etc/cont-init.d/56-brave-set-prefs-from-env.sh
+++ rootfs/etc/cont-init.d/56-brave-set-prefs-from-env.sh
@@ -6,4 +6,12 @@ set -u # Treat unset variables as an error.
 # Make sure the Brave profile directory exists.
 mkdir -p /config/profile/Default
 
+# Disable the session restore bubble by setting the exit type to normal.
+LOCAL_STATE="/config/profile/Local State"
+if [ -f "$LOCAL_STATE" ]; then
+    # Set exit type to Normal to prevent restore dialog.
+    sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/g' "$LOCAL_STATE"
+    sed -i 's/"exit_type": "Crashed"/"exit_type": "Normal"/g' "$LOCAL_STATE"
+fi
+
 # vim:ft=sh:ts=4:sw=4:et:sts=4
